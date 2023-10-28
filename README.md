0160c39fb1934e4c5546334c5382c6b0339dc472
Manhhung2199 <146406661+hungkafi@users.noreply.github.com>
2023-10-28 13:08:20 +0700

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# norstar
+
+sass --watch app/scss:app/dist
---
+++ app/js/swiper.js
@@ -0,0 +1,168 @@
+var swiper = new Swiper(".sl-topbar", {
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 70,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        400: {
+            slidesPerView: 1,
+        },
+        600: {
+            slidesPerView: 2,
+        },
+        750: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1280: {
+            slidesPerView: 2,
+            spaceBetween: 70,
+        },
+    },
+    // autoplay: {
+    //     delay: 100,
+    // },
+    speed: 3000,
+});
+
+var swiper = new Swiper(".tf-menu-slider", {
+    slidesPerView: 1,
+    loop: false,
+    spaceBetween: 0,
+    navigation: {
+        nextEl: ".btn-nav.btn-next",
+        prevEl: ".btn-nav.btn-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        400: {
+            slidesPerView: 1,
+        },
+        600: {
+            slidesPerView: 2,
+        },
+        750: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3
+        },
+        1280: {
+            slidesPerView: 4
+        },
+    },
+});
+
+var swiper = new Swiper(".tf-testimonial-slider", {
+    slidesPerView: 1,
+    loop: false,
+    spaceBetween: 0,
+    navigation: {
+        nextEl: ".btn-testimonial.btn-next",
+        prevEl: ".btn-testimonial.btn-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        400: {
+            slidesPerView: 1,
+        },
+        600: {
+            slidesPerView: 1,
+        },
+        750: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 2
+        },
+        1280: {
+            slidesPerView: 2
+        },
+    },
+});
+
+var swiper = new Swiper(".tf-blog-slider", {
+    slidesPerView: 1,
+    loop: false,
+    spaceBetween: 0,
+    navigation: {
+        nextEl: ".btn-blog.btn-next",
+        prevEl: ".btn-blog.btn-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        400: {
+            slidesPerView: 1,
+        },
+        600: {
+            slidesPerView: 1,
+        },
+        750: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 2
+        },
+        1280: {
+            slidesPerView: 3
+        },
+    },
+});
+
+var swiper = new Swiper(".tf-partner-slider", {
+    slidesPerView: 1,
+    loop: false,
+    spaceBetween: 0,
+    navigation: {
+        nextEl: ".btn-blog.btn-next",
+        prevEl: ".btn-blog.btn-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        400: {
+            slidesPerView: 1,
+        },
+        600: {
+            slidesPerView: 3,
+        },
+        750: {
+            slidesPerView: 4,
+        },
+        991: {
+            slidesPerView: 5
+        },
+        1280: {
+            slidesPerView: 6
+        },
+    },
+});
+
